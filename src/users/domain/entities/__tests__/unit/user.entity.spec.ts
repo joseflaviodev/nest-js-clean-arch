940639ae0125f2c1679d82ab50a4ec7d9144b989
José Flávio <jose.flavio@adaptweb.com.br>
2024-10-04 20:34:58 -0300

    Update methods unit tests

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -23,6 +23,13 @@ describe("UserEntity unit tests", () => {
     expect(typeof sut.name).toBe('string');
   });
 
+  it('Setter of name field', () => {
+    sut['name'] = 'other name';
+    expect(sut.name).toEqual('other name');
+    expect(typeof sut.name).toBe('string');
+  });
+
+
   it('Getter of email field', () => {
     expect(sut.email).toBeDefined();
     expect(sut.email).toEqual(props.email);
@@ -35,8 +42,24 @@ describe("UserEntity unit tests", () => {
     expect(typeof sut.password).toBe('string');
   });
 
+  it('Setter of password field', () => {
+    sut['password'] = 'other password';
+    expect(sut.password).toEqual('other password');
+    expect(typeof sut.password).toBe('string');
+  });
+
   it('Getter of createdAt field', () => {
     expect(sut.createdAt).toBeDefined();
     expect(sut.createdAt).toBeInstanceOf(Date);
+  });
+
+  it('Should update a user', () => {
+    sut.update('other name');
+    expect(sut.name).toEqual('other name');
+  });
+
+  it('Should update the password field', () => {
+    sut.updatePassword('other password');
+    expect(sut.password).toEqual('other password');
   });
 });
